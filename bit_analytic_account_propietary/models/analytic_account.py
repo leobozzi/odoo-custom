907dfcfd7132e925dd56ccc6bee56f979de27924
leobozzi <lbozzi@gmail.com>
2023-01-12 11:18:50 -0300

    update

--- bit_analytic_account_propietary/models/analytic_account.py
+++ bit_analytic_account_propietary/models/analytic_account.py
@@ -21,4 +21,41 @@ class AnalyticAccount(models.Model):
         string="Encabezado",
     )
 
+    dynamic_description = fields.Text(
+        string="Dynamic Description",
+    )
 
+    @api.onchange('description')
+    def _onchange_description(self):
+        description = ''
+        #DIA#
+        description = self.description.replace("#DIA#", str(self.date.day))
+        #MES#
+        if self.date.month == 1:
+            description = description.replace("#MES#", "ENERO")
+        elif analytic.date.month == 2:
+            description = description.replace("#MES#", "FEBRERO")
+        elif analytic.date.month == 2:
+            description = description.replace("#MES#", "MARZO")
+        elif analytic.date.month == 2:
+            description = description.replace("#MES#", "ABRIL")
+        elif analytic.date.month == 2:
+            description = description.replace("#MES#", "MAYO")
+        elif analytic.date.month == 2:
+            description = description.replace("#MES#", "JUNIO")
+        elif analytic.date.month == 2:
+            description = description.replace("#MES#", "JULIO")
+        elif analytic.date.month == 2:
+            description = description.replace("#MES#", "AGOSTO")
+        elif analytic.date.month == 2:
+            description = description.replace("#MES#", "SEPTIEMBRE")
+        elif analytic.date.month == 2:
+            description = description.replace("#MES#", "OCTUBRE")
+        elif analytic.date.month == 2:
+            description = description.replace("#MES#", "NOVIEMBRE")
+        elif analytic.date.month == 2:
+            description = description.replace("#MES#", "DICIEMBRE")
+        #AÑO#
+        description = description.replace("#AÑO#", str(self.date.year))
+        
+        self.dynamic_description = description
